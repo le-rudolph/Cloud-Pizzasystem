c161ebfeef0652c323d00dfeb917bf5f89974208
Lennard Denis Rudolph <lennard.d.rudolph@hochschule-stralsund.de>
2024-06-13 08:21:18 +0200

    wip dockerize lieferantenservice

---
+++ lieferantenservice/src/lieferantenservice.py
@@ -0,0 +1,42 @@
+import pika
+
+QUEUE_HOST = 'localhost'
+
+
+def receive_order(channel, method, properties, body):
+    print(channel, method, properties, body)
+    print(f"received order {body}")
+
+
+def main():
+    print("connecting to queue")
+    connection = None
+    retries = 5
+    while retries > 0:
+        try:
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=QUEUE_HOST))
+        except pika.exceptions.AMQPConnectionError:
+            retries -= 1
+            print(f"queue is down, {retries} remaining")
+
+    if connection is None:
+        raise RuntimeError("could not connect to queue")
+        
+    channel = connection.channel()
+
+    channel.queue_declare(
+        queue="order", on_message_callback=receive_order, auto_ack=True)
+
+    print("waiting for order")
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    print("launching lieferantenservice")
+    try:
+        main()
+    except pika.exceptions.AMQPConnectionError as e:
+        print("queue is down")
+    except KeyboardInterrupt:
+        print("goodbye")
