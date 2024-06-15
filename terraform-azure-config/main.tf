f397f8bc714ff226d618282f4cf92219cb3c6462
Lennard Denis Rudolph <lennard.d.rudolph@hochschule-stralsund.de>
2024-06-16 00:49:34 +0200

    wip tf config

--- terraform-azure-config/main.tf
+++ terraform-azure-config/main.tf
@@ -1,20 +1,34 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-
-  required_version = ">= 1.8.0"
+// random names for unique azure resources
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
-provider "azurerm" {
-  features {}
+// resource group to be created
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
 }
 
-resource "azurerm_resource_group" "pizza" {
-  name     = "cloudPizzasystem"
-  location = "ger-west-central"
+// virtual network for the cluster
+resource "random_pet" "azurerm_virtual_network_name" {
+  prefix = "vnet"
+}
+
+resource "azurerm_virtual_network" "pizza" {
+  name                = random_pet.azurerm_virtual_network_name.id
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+// subnet in the virtual network
+resource "random_pet" "azurerm_subnet_name" {
+  prefix = "sub"
+}
+
+resource "azurerm_subnet" "pizza" {
+  name                 = random_pet.azurerm_subnet_name.id
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.pizza.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
---
+++ terraform-azure-config/providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 1.8.0"
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
