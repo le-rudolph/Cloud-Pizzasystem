89b7131c082b7816537661ba2cb32a4e3da01a22
ToxicBeer <erik.schippmann@hochschule-stralsund.de>
2024-06-19 11:13:53 +0200

    produktservice frontend

--- Produktservice/app.js
+++ Produktservice/app.js
@@ -1,33 +1,45 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require("mongoose");
 const { Pizza } = require('./model');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/pizza', async (req, res) => {
-  const allPizza = await Pizza.find()
-  console.log(allPizza)
-  return res.status(200).json(allPizza)
-})
+  const allPizza = await Pizza.find();
+  console.log(allPizza);
+  const mappedPizza = {};
+  allPizza.forEach((pizza) => {
+    mappedPizza[pizza.Name] = {
+    };
+
+    pizza.S && (mappedPizza[pizza.Name].S = pizza.S);
+    pizza.M && (mappedPizza[pizza.Name].M = pizza.M);
+    pizza.L && (mappedPizza[pizza.Name].L = pizza.L);
+  });
+
+  console.log(mappedPizza);
+
+  return res.status(200).json(mappedPizza);
+});
 
 app.post("/create_produkt", async (req, res) => {
-    console.log(req.body)
-    const newPizza = new Pizza({...req.body})
-    const insertedPizza = await newPizza.save()
-    res.status(200).json(insertedPizza)
-})
+  console.log(req.body);
+  const newPizza = new Pizza({ ...req.body });
+  const insertedPizza = await newPizza.save();
+  res.status(200).json(insertedPizza);
+});
 
 const start = async () => {
-  await mongoose.connect('mongodb://mongo:27017/pizza')
+  await mongoose.connect('mongodb://mongo:27017/pizza');
 
   app.listen(port, () => {
-    console.log(`Produktservice listening on port ${port}`)
-  })
-}
+    console.log(`Produktservice listening on port ${port}`);
+  });
+};
 
-start()
+start();
---
+++ Produktservice/public/script.js
@@ -0,0 +1,63 @@
+window.onload = () => {
+    showPizza();
+};
+
+function showPizza() {
+    const container = document.getElementById("allPizza");
+    container.textContent = "";
+
+    fetch("/pizza")
+        .then((res) => res.json())
+        .then((body) => {
+            console.log(body);
+            if (!body) return;
+
+            Object.keys(body).forEach((pizza) => {
+                const spreis = body[pizza].S;
+                const mpreis = body[pizza].M;
+                const lpreis = body[pizza].L;
+
+                const newDiv = document.createElement("div");
+                newDiv.classList.add("pizza-container")
+                const name = document.createElement("span");
+                name.innerHTML = pizza;
+                const s = document.createElement("span");
+                if (spreis) s.innerHTML = "S: " + spreis;
+                const m = document.createElement("span");
+                if (mpreis) m.innerHTML = "M: " + mpreis;
+                const l = document.createElement("span");
+                if (lpreis) l.innerHTML = "L: " + lpreis;
+
+                newDiv.append(name, s, m, l);
+                container.append(newDiv);
+            });
+        });
+}
+
+function onSubmit(form, e) {
+    e.preventDefault();
+    document.getElementById("hinweis").style = "display: none;";
+    document.getElementById("error").style = "display: none;";
+
+    const data = {
+        Name: form.name.value,
+        S: form.spreis.value,
+        M: form.mpreis.value,
+        L: form.lpreis.value,
+    };
+
+    fetch("/create_produkt", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+    }).then((res) => {
+        if (res.ok) {
+            document.getElementById("hinweis").style = "display: inherit;";
+            showPizza();
+        } else {
+            document.getElementById("error").style = "display: inherit;";
+        }
+    });
+
+    return false;
+}
